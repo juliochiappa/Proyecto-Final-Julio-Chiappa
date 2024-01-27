745371d746a210ebe720bfb47a2e7f25bf96aea6
julio chiappa <juliochiappa43@gmail.com>
2024-01-27 15:11:04 -0300

    Trabajo Final de Java Script

---
+++ js/app.js
@@ -0,0 +1,131 @@
+import Swal from "sweetalert2";
+import tareasApi from "../src/api/tareas.api";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const turnosLista = document.getElementById('listaTurnos');
+
+    // Función para mostrar pacientes desde el servidor utilizando la Api respectiva (GET)
+    const mostrarPacientes = async () => {
+            const turnos = await tareasApi.obtenerPacientes();
+            console.log(turnos);
+
+            // Mostrar la lista de pacientes
+            actualizarTurnos(turnos);
+    };
+
+    // Función para actualizar estado de la lista de turnos
+    const actualizarTurnos = (turnos) => {
+        turnosLista.innerHTML = '';
+
+        turnos.forEach((paciente, estadoActual) => {
+            const li = document.createElement('li');
+            li.className = 'lista';
+            li.textContent = `${paciente.nombre} ${paciente.apellido} - Estado: ${paciente.estado}`;
+
+            const botonAtendido = document.createElement('button');
+            botonAtendido.className = 'btn btn-info btn-sm mx-5 d-block m-2';
+            botonAtendido.textContent = 'Paciente Atendido';
+            botonAtendido.onclick = () => cambiarEstado(paciente.id, estadoActual, 'Paciente Atendido');
+
+            const botonEliminar = document.createElement('button');
+            botonEliminar.className = 'btn btn-danger btn-sm mx-5 d-block m-2';
+            botonEliminar.textContent = 'Eliminar Paciente';
+            botonEliminar.onclick = () => eliminarPaciente(paciente.id);
+
+            li.appendChild(botonAtendido);
+            li.appendChild(botonEliminar);
+
+            turnosLista.appendChild(li);
+        });
+    };
+
+    // Función para ingresar un nuevo paciente
+    window.ingresarPaciente = async () => {
+        const nombreInput = document.getElementById('nombre');
+        const apellidoInput = document.getElementById('apellido');
+
+        const nombre = nombreInput.value.trim();
+        const apellido = apellidoInput.value.trim();
+
+        if (nombre && apellido) {
+            const paciente = {
+                nombre,
+                apellido,
+                estado: 'Paciente en Espera',
+            };
+
+                // Agregar paciente al servidor utilizando la Api respectiva (POST)
+                await tareasApi.agregarPacientes(paciente);
+
+                Swal.fire({
+                    title: "¡El paciente se encuentra registrado en Espera!",
+                    icon: "success",
+                });
+
+                // Actualizar la lista de pacientes
+                mostrarPacientes();
+
+                // Limpiar campos para nuevo paciente
+                nombreInput.value = '';
+                apellidoInput.value = '';
+        }
+    };
+   
+    // Función para cambiar el estado del paciente del servidor utilizando la Api respectiva (PUT)
+    const cambiarEstado = async (idPaciente, estadoActual, nuevoEstado) => {
+        const paciente = turnos[estadoActual];
+        paciente.estado = nuevoEstado;
+            
+        // Verificar si el paciente está en estado "Paciente Atendido"
+        if (paciente.estado === 'Paciente Atendido') {
+            
+            // Actualizar el estado del paciente en el servidor
+            await tareasApi.actualizarEstadoPaciente(idPaciente, nuevoEstado);
+    
+            // Mostrar un mensaje de éxito con SweetAlert
+            Swal.fire({
+                title: `¡El paciente ${paciente.nombre} ${paciente.apellido} fue cambiado al estado de ${nuevoEstado}!`,
+                icon: "success",
+            });
+    
+            // Actualizar la lista de pacientes desde el servidor
+            mostrarPacientes();
+         } 
+    };
+
+    // Función para eliminar un paciente del servidor utilizando la Api respectiva (DELETE)
+    const eliminarPaciente = async (idPaciente) => {
+        Swal.fire({
+            title: "¿Estás seguro que deseas eliminar el Paciente?",
+            icon: "question",
+            showCancelButton: true,
+            confirmButtonText: "Si",
+            cancelButtonText: "No",
+            cancelButtonColor: "red",
+            confirmButtonColor: "green",
+        }).then(async (resp) => {
+            if (resp.isConfirmed) {
+                await tareasApi.eliminarPacientes(idPaciente);
+
+                Swal.fire({
+                    title: "¡El paciente fue eliminado de la lista!",
+                    icon: "success",
+                    showConfirmButton: false,
+                    timer: 1000,
+                });
+
+                mostrarPacientes();
+            }
+        });
+    };
+
+    // Inicializar la aplicación
+    mostrarPacientes();
+
+});
+ 
+
+
+
+
+
---
+++ src/api/tareas.api.js
@@ -0,0 +1,73 @@
+//OBTENER PACIENTE METODO  GET
+
+const obtenerPacientes = async () => {
+    const resp = await fetch("http://localhost:3000/pacientes")
+    const data = await resp.json();
+    console.log(data);
+    return data;
+}
+
+//OBTENER PACIENTE METODO GET POR ID
+
+const obtenerPacientesId = async (id) => {
+    const resp = await fetch(`http://localhost:3000/pacientes/${id}`);
+    const data = await resp.json();
+    console.log(data);
+    return data;
+
+}
+
+//AGREGAR PACIENTE METODO  POST
+
+const agregarPacientes = async (paciente) => {
+    const resp = await fetch("http://localhost:3000/pacientes", {
+        method: "POST",
+        body: JSON.stringify(paciente),
+        headers: {
+            "Content-type": "application/json; charset-UTF 8"
+        },
+    });
+    const data = await resp.json();
+    console.log(data);
+    return data;
+}
+
+//ACTUALIZAR PACIENTE METODO  PUT
+
+const actualizarEstadoPaciente = async (id, paciente, estadoNuevo,) => {
+    // Modificar el objeto paciente con el nuevo estado
+    paciente.estado = estadoNuevo;
+    
+    const resp = await fetch(`http://localhost:3000/pacientes/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(paciente),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+    });
+    const data = await resp.json();
+    console.log(data);
+    return data;
+};
+
+//ELIMINAR PACIENTE METODO  DELETE
+
+const eliminarPacientes = async (id) => {
+    const resp = await fetch(`http://localhost:3000/pacientes/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json; charset-UTF 8"
+        },
+    });
+    const data = await resp.json();
+    console.log(data);
+    return data;
+}
+
+export default {
+    obtenerPacientes,
+    obtenerPacientesId,
+    agregarPacientes,
+    actualizarEstadoPaciente,
+    eliminarPacientes,
+}
